b683d180f6bf5a6c7d181d4e958a42a9ba77dcf7
Oche Ejeh <ocheejeh1@gmail.com>
2023-02-19 16:19:46 -0800

    print program using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - prints Programming is like building a multilingual puzzle\n
+*
+* Return: 0 when program is Success
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return(0);
+}
