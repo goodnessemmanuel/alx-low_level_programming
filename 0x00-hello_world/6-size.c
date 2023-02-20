38ae2ba180d28ae1746f2ff8b11935a9e7b7b4b7
Oche Ejeh <ocheejeh1@gmail.com>
2023-02-19 17:42:09 -0800

    print data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,7 @@ int main(void)
 	long c;
 	long long d;
 	float e;
+
 	printf("Size of a char: %lu byte(s)\n", sizeof(a));
 	printf("Size of an int: %ld byte(s)\n", sizeof(b));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(c));
