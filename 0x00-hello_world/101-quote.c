ce17214883a715bdb358b30bb245c143647fd6c1
Oche Ejeh <ocheejeh1@gmail.com>
2023-02-19 18:02:40 -0800

    print to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - prints output using printf function
+*
+* Return: 0 when program is Success
+*/
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-1e9\n");
+	return (0);
+}
