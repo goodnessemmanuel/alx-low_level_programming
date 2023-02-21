12e29c70cdd6dc6efc1475ca066022fc282a86c7
Oche Ejeh <ocheejeh1@gmail.com>
2023-02-21 15:44:39 -0800

    remove main func call

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,6 @@
 #include "main.h"
 
 /**
-* main - prints alphabet_
-* Return: (0) on success
-* Description: call custom function
-*/
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
 * print_alphabeth - implementing print_alphabet prototype
 * Retun: void
 * Description: Prints all small letters of the alphabet
