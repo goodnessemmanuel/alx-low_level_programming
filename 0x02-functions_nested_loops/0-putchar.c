28c135f07bf0d9ba10b76800d52ab5ab94c1e73e
Oche Ejeh <ocheejeh1@gmail.com>
2023-02-21 12:32:23 -0800

    program to print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - prints '_putchar_
+* Retun: 0 on success
+* Description: Prints _putchar to the standard output
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
