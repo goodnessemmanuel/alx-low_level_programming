0112074af8fb5b18d8a2234c98f36f8bc2855428
Oche Ejeh <ocheejeh1@gmail.com>
2023-02-21 18:33:56 -0800

    print last digit of num

---
+++ 0x02-functions_nested_loops/7-last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* print_last_digit - last digit
+* @n: the number to check
+* Return: result according to the undely description
+* Description: prints the last digit of n
+*/
+int print_last_digit(int n)
+{
+	_putchar(_abs(n % 10));
+	return (0);
+}
