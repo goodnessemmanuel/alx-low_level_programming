8c80b70f3e739d338dacc9114edf21c7c7d65913
Oche Ejeh <ocheejeh1@gmail.com>
2023-02-21 17:59:27 -0800

    fix betty styling

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _print_sign - prints the sign of a number
+* print_sign - prints the sign of a number
 * @n: the number to check
 * Return: result according to the undely description
 * Description:
