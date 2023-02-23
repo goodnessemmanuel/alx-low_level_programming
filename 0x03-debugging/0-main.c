d3ce7a1144b66625910873567166b01cff965831
Oche Ejeh <ocheejeh1@gmail.com>
2023-02-23 11:31:57 -0800

    debugging with test

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - tests positive_or_negative function for zero input
+* Return: 0
+* Description: tests that positive_or_negative gives the
+* correct output for zero
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
