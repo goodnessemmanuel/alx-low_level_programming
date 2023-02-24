96a4460cba1fd1546333f9add840222754517245
Oche Ejeh <ocheejeh1@gmail.com>
2023-02-23 17:02:46 -0800

    multiply function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Test for digit character.
+ * @a : first input
+ * @b : second input
+ * Return: Always a multiply by b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
