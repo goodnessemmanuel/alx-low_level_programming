78e4762c594a3e67b656051a12281460c0fffbc9
Oche Ejeh <ocheejeh1@gmail.com>
2023-03-04 11:15:25 -0800

    function to update value of a pointer variable to point to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Resets a pointer value to 98.
+ * @n : address to memory of pointer to reset value
+ * Return: no value is returned.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+# C Pointers
+
+### how to compile
+```
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-reset_to_98.c -o 0-98
+```
