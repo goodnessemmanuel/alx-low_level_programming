2becbf4b755c54ab9ad29a08693e788e57cc1e38
Oche Ejeh <ocheejeh1@gmail.com>
2023-03-05 10:30:43 -0800

    print string in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_rev - Prints a string in reverse order
+ * @s: pointer to first char in the string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int len;
+	char *sm = s;
+
+	len = 0;
+	while (*sm != '\0')
+	{
+		sm++;
+		len++;
+	}
+
+	while (len > -1)
+	{
+		putchar(*(s + len));
+		len--;
+	}
+	putchar('\n');
+}
