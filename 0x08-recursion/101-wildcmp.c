14da97d72555f18aab836cff76a3a5ba94b4c3fe
Oche Ejeh <ocheejeh1@gmail.com>
2023-03-09 17:59:29 -0800

    wild compare function

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "main.h"
 /**
  * wildcmp - Entry Point
@@ -8,5 +9,19 @@
  */
 int wildcmp(char *s1, char *s2)
 {
+	if (*s1 != '\0' && *s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+
+	if (*s1 == '\0' && *s2 == '*')
+		return (wildcmp(s1, s2 + 1));
+
+	if (*s1 == *s2 && *s2 != '\0' && *s2 != '\0')
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s1 == *s2 && *s1 == '\0' && *s2 == '\0')
+		return (1);
+
 	return (0);
 }
+
+
